b1fb021d76ac34fc719d5f026e922d8af7cb144c
Abbos <webdevel1212@gmail.com>
2022-02-24 20:57:52 +0500

    boshlangich

--- js/index.js
+++ js/index.js
@@ -1,4 +1,50 @@
 "use strict";
-console.log('abbos');
-let a = 5;
-let b = 6;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const prompt = (0, prompt_sync_1.default)();
+// let  x = +prompt("x:")
+// let y = 7*x+3*x+6
+// console.log(y)
+// let pro = prompt("izoh:")
+//  function prohtika(){
+//     if(pro == "abbos"){
+//         return true
+//     } 
+//     return false
+//  }
+//  let funct = prohtika()
+//  console.log(funct)
+// for(let i = 20; i<35; i++){
+//      console.log(i)
+// }
+// let n = +prompt('n')
+//   for(let i=0; i<n;i++){
+//       console.log(i)
+//   }
+//  let i = +prompt("i:")
+// for(let i=n;i<10;i++){
+//     for(let a=0;a<n;a++){
+//         console.log(a+"*"+i+"="+a*i)
+//     }
+// }
+// for(let i = 0;i < 10; i++){
+//     for(let a = 0;a < 10;a++){
+//         console.log(a+' * '+i+' = '+a*i)
+//     }
+//     console.log('      ')
+// }
+// var a = prompt("a:")
+// function hello(){
+//     return "Hello " +a
+// }
+// let func = hello()
+// console.log(func)
+let i = 2;
+let b = "abbos";
+while (i < 4) {
+    console.log(i + " " + b + " " + " kichkina ku bular");
+    i++;
+}
